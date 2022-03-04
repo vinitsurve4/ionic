41f81cd8f8310152843fb3f34217e54f729b3709
Volt <vinitsurve4@gmail.com>
2022-03-04 11:46:46 +0530

    init

--- ionic-app/src/app/app-routing.module.ts
+++ ionic-app/src/app/app-routing.module.ts
@@ -5,6 +5,18 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'menu',
+    loadChildren: () => import('./pages/menu/menu.module').then( m => m.MenuPageModule)
+  },
+  {
+    path: 'first',
+    loadChildren: () => import('./pages/first/first.module').then( m => m.FirstPageModule)
+  },
+  {
+    path: 'second',
+    loadChildren: () => import('./pages/second/second.module').then( m => m.SecondPageModule)
   }
 ];
 @NgModule({
